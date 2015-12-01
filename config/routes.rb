f377a7bc9d4236e6d5c1e1394655a1772c781acb
Heda Hahn <hedassecret@gmail.com>
2015-12-01 14:45:36 -0800

    Welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+  get "welcome/index"
 
-<<<<<<< HEAD
-  get 'welcome/faq'
-=======
-  get 'welcome/contact'
->>>>>>> master
+  get "welcome/about"
 
-  root to: 'welcome#index'
+  root 'welcome#about'
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -65,4 +61,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
