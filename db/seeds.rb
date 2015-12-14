796e4a3284376345be28c3c78978de6df211163f
Heda Hahn <hedassecret@gmail.com>
2015-12-14 15:07:14 -0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,23 @@ include RandomData
 
  # Create Posts
  50.times do
- # #1
    Post.create!(
- # #2
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
 
- # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
+
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "A unique title", body: "A unique body")
+ puts "#{Post.count}"
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
