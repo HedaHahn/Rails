3d696a49a5f1025803903560c558861e3fa94ce0
Elisabeth <heda@hedahahn.com>
2016-01-14 23:25:12 -0800

    32 NEW

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'advertisements/index'
-
-  get 'advertisements/show'
-
-  get 'advertisements/new'
-
-  get 'advertisements/create'
-
+  resources :advertisements
   resources :posts
 
   get 'index' => 'welcome#index'
