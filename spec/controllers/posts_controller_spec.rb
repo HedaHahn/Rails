499b7fc1d301492fdc898f38ecaa5215965755da
Elisabeth <heda@hedahahn.com>
2016-01-21 13:03:20 -0800

    Checkpoint 33

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -133,7 +133,9 @@ RSpec.describe PostsController, type: :controller do
         if @post.destroy
           flash[:notice] = "\"#{@post.title}\" was deleted successfully."
           redirect_to posts_path
+
         else
           flash.now[:alert] = "There was an error deleting the post."
           render :show
         end
+      end
