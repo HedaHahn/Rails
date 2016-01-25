af435f643bcd945450b3b72ca976b2d86c52eaa8
Elisabeth <heda@hedahahn.com>
2016-01-24 18:10:21 -0800

    Checkpoint 33

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -35,107 +35,107 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-    describe "POST create" do
-      it "increases the number of Post by 1" do
+  describe "POST create" do
+    it "increases the number of Post by 1" do
         expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
-      end
+    end
 
-      it "assigns the new post to @post" do
-        post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-        expect(assigns(:post)).to eq Post.last
-      end
+    it "assigns the new post to @post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(assigns(:post)).to eq Post.last
+    end
 
-      it "redirects to the new post" do
-        post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-        expect(response).to redirect_to Post.last
+    it "redirects to the new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to Post.last
+    end
+  end
+
+  describe "GET show" do
+     it "returns http success" do
+       get :show, {id: my_post.id}
+       expect(response).to have_http_status(:success)
+     end
+
+     it "renders the #show view" do
+       get :show, {id: my_post.id}
+       expect(response).to render_template :show
+     end
+
+     it "assigns my_post to @post" do
+       get :show, {id: my_post.id}
+       expect(assigns(:post)).to eq(my_post)
+     end
+   end
+
+   describe "GET edit" do
+     it "returns http success" do
+        get :edit, {id: my_post.id}
+        expect(response).to have_http_status(:success)
+     end
+
+     it "renders the #edit view" do
+        get :edit, {id: my_post.id}
+        expect(response).to render_template :edit
+     end
+
+     it "assigns post to be updated to @post" do
+        get :edit, {id: my_post.id}
+
+        post_instance = assigns(:post)
+        expect(post_instance.id).to eq my_post.id
+        expect(post_instance.title).to eq my_post.title
+        expect(post_instance.body).to eq my_post.body
       end
     end
 
-    describe "GET show" do
-       it "returns http success" do
-         get :show, {id: my_post.id}
-         expect(response).to have_http_status(:success)
-       end
+    describe "PUT update" do
+      it "updates post with expected attributes" do
+        new_title = RandomData.random_sentence
+        new_body = RandomData.random_paragraph
 
-       it "renders the #show view" do
-         get :show, {id: my_post.id}
-         expect(response).to render_template :show
-       end
+        put :update, id: my_post.id, post: {title: new_title, body: new_body}
 
-       it "assigns my_post to @post" do
-         get :show, {id: my_post.id}
-         expect(assigns(:post)).to eq(my_post)
-       end
-     end
-
-     describe "GET edit" do
-       it "returns http success" do
-          get :edit, {id: my_post.id}
-          expect(response).to have_http_status(:success)
-        end
-
-        it "renders the #edit view" do
-          get :edit, {id: my_post.id}
-          expect(response).to render_template :edit
-        end
-
-        it "assigns post to be updated to @post" do
-          get :edit, {id: my_post.id}
-
-          post_instance = assigns(:post)
-          expect(post_instance.id).to eq my_post.id
-          expect(post_instance.title).to eq my_post.title
-          expect(post_instance.body).to eq my_post.body
-        end
+        updated_post = assigns(:post)
+        expect(updated_post.id).to eq my_post.id
+        expect(updated_post.title).to eq new_title
+        expect(updated_post.body).to eq new_body
       end
 
-      describe "PUT update" do
-        it "updates post with expected attributes" do
-          new_title = RandomData.random_sentence
-          new_body = RandomData.random_paragraph
+      it "redirects to the updated post" do
+        new_title = RandomData.random_sentence
+        new_body = RandomData.random_paragraph
 
-          put :update, id: my_post.id, post: {title: new_title, body: new_body}
+        put :update, id: my_post.id, post: {title: new_title, body: new_body}
 
-          updated_post = assigns(:post)
-          expect(updated_post.id).to eq my_post.id
-          expect(updated_post.title).to eq new_title
-          expect(updated_post.body).to eq new_body
-        end
+        expect(response).to redirect_to my_post
+      end
+    end
 
-        it "redirects to the updated post" do
-          new_title = RandomData.random_sentence
-          new_body = RandomData.random_paragraph
+    describe "DELETE destroy" do
+      it "deletes the post" do
+        delete :destroy, {id: my_post.id}
 
-          put :update, id: my_post.id, post: {title: new_title, body: new_body}
-
-          expect(response).to redirect_to my_post
-        end
+        count = Post.where({id: my_post.id}).size
+        expect(count).to eq 0
       end
 
-      describe "DELETE destroy" do
-        it "deletes the post" do
-          delete :destroy, {id: my_post.id}
+      it "redirects to posts index" do
+        delete :destroy, {id: my_post.id}
 
-          count = Post.where({id: my_post.id}).size
-          expect(count).to eq 0
-        end
-
-        it "redirects to posts index" do
-          delete :destroy, {id: my_post.id}
-
-          expect(response).to redirect_to posts_path
-        end
+        expect(response).to redirect_to posts_path
       end
+    end
 
-      def destroy
-        @post = Post.find(params[:id])
+    def destroy
+      @post = Post.find(params[:id])
 
-        if @post.destroy
-          flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-          redirect_to posts_path
+      if @post.destroy
+        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+        redirect_to posts_path
 
-        else
-          flash.now[:alert] = "There was an error deleting the post."
-          render :show
-        end
+      else
+        flash.now[:alert] = "There was an error deleting the post."
+        render :show
       end
+    end
