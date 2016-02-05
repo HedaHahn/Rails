b69328eb8b1d38d6d304f41ff9e47086c9a0b9f1
Elisabeth <heda@hedahahn.com>
2016-02-04 23:19:37 -0800

    Checkpoint 32

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'advertisements/index'
-
-  get 'advertisements/show'
-
-  get 'advertisements/new'
-
-  get 'advertisements/create'
-
-  resources :post
+  resources :posts
 
   get 'about' => 'welcome#about'
 
