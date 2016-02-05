33d6b73d4fba55b2477c831e9150959b3a5985bd
Elisabeth <heda@hedahahn.com>
2016-02-05 02:24:32 -0800

    Checkpoint 33

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,36 @@
 class PostsController < ApplicationController
+   def index
+     @posts = Post.all
+   end
 
-  def index
-    @posts = Post.all
-  end
+   def create
 
-  def show
-    @post = Post.find(params[:id])
-  end
+     @post = Post.new
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
 
-  def new
-    @post = Post.new
-  end
+     if @post.save
 
-  def create
+       flash[:notice] = "Post was saved."
+       redirect_to @post
+     else
 
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :new
+     end
+   end
 
-    if @post.save
-      flash[:notice] = "Post was saved."
-      redirect_to @post
-    else
+   def show
+   end
 
-      flash.now[:alert] = "There was an error saving the post. Please try again."
-      render :new
-    end
-  end
+   def new
+   end
 
-  def edit
-      @post = Post.find(params[:id])
-  end
+   def edit
+     @post = Post.find(params[:id])
+   end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
-
-    if @post.save
-      flash[:notice] = "Post was updated."
-      redirect_to @post
-    else
-      flash.now[:alert] = "There was an error saving the post. Please try again."
-      render :edit
-    end
-  end
-end
+   def faq
+   end
+   
+ end
