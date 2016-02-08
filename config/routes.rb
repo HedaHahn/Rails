4a03476cd0adf834d6eae565060a33de23041b14
Elisabeth <heda@hedahahn.com>
2016-02-08 10:34:40 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
 
-  root 'welcome#about'
+  root to: 'welcome#about'
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
