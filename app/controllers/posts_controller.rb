9efa908afb0d2feb769d3fd614199803ecc9b320
Elisabeth <heda@hedahahn.com>
2016-02-21 20:21:27 -0800

    Checkpoint 34

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,8 @@ class PostsController < ApplicationController
     @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
 
+    @post.topic = @topic
+
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe PostsController, type: :controller do
 
   describe "GET new" do
     it "returns http success" do
+      get :new, topic_id: my_topic.id
       expect(response).to have_http_status(:success)
     end
 
@@ -24,30 +25,33 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST create" do
     it "increases the number of Post by 1" do
-      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+      expect{post :create, topic_id: my_topic.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
     end
 
     it "assigns the new post to @post" do
-      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      post :create, topic_id: my_topic.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
       expect(assigns(:post)).to eq Post.last
     end
 
     it "redirects to the new post" do
-      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-      expect(response).to redirect_to Post.last
+      post :create, topic_id: my_topic.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to [my_topic, Post.last]
     end
   end
 
     describe "GET show" do
        it "returns http success" do
+         get :show, topic_id: my_topic.id, id: my_post.id
          expect(response).to have_http_status(:success)
        end
 
        it "renders the #show view" do
+         get :show, topic_id: my_topic.id, id: my_post.id
          expect(response).to render_template :show
        end
 
        it "assigns my_post to @post" do
+         get :show, topic_id: my_topic.id, id: my_post.id
          expect(assigns(:post)).to eq(my_post)
        end
      end
@@ -82,8 +86,8 @@ RSpec.describe PostsController, type: :controller do
 
         it "assigns post to be updated to @post" do
           get :edit, topic_id: my_topic.id, id: my_post.id
-
           post_instance = assigns(:post)
+
           expect(post_instance.id).to eq my_post.id
           expect(post_instance.title).to eq my_post.title
           expect(post_instance.body).to eq my_post.body
